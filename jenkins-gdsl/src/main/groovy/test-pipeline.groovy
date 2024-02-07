d8a3f4310ab5ed20ef0299b8ded1a97fe1557169
Manimaran Balagangadharan <manimaranb@gmail.com>
2024-02-06 23:56:39 -0800

    read Json

--- jenkins-gdsl/src/main/groovy/test-pipeline.groovy
+++ jenkins-gdsl/src/main/groovy/test-pipeline.groovy
@@ -2,8 +2,10 @@ pipeline{
     agent any
     stages{
         stage ('Read File'){
-            def props = readJSON file: '/jenkins-gdsl/src/main/groovy/org/example/properties.json'
-            print props['name']
+            script{
+                def props = readJSON file: '/jenkins-gdsl/src/main/groovy/org/example/properties.json'
+                print props['name']
+            }
         }
     }
 }
