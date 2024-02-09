4178e766179e218235cbf5fa6a0e12b0fc7d27fd
Manimaran Balagangadharan <manimaranb@gmail.com>
2024-02-08 19:52:49 -0800

    rem comment

--- jenkins-gdsl/src/main/groovy/test-pipeline.groovy
+++ jenkins-gdsl/src/main/groovy/test-pipeline.groovy
@@ -7,6 +7,8 @@ pipeline{
                     print "Currently in: " + pwd()
                     def props = readJSON file: '\\jenkins-gdsl\\src\\main\\groovy\\org\\example\\properties.json'
                     print props['name']
+
+                    //test
                 }
             }
         }
